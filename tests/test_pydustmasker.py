27853d6b8f8574e53cb113d489540b0fb1d4a18d
Antonio Camargo <antoniop.camargo@gmail.com>
2025-12-15 13:57:11 -0300

    Update test_pydustmasker.py

--- tests/test_pydustmasker.py
+++ tests/test_pydustmasker.py
@@ -6,7 +6,7 @@ def test_dust_masker_creation():
     assert masker.sequence == "TACCCCCCCGCGTTTTTTT"
     assert masker.window_size == 64
     assert masker.score_threshold == 20
-    assert masker.intervals == [(2, 9), (12, 19)]
+    assert masker.intervals == ((2, 9), (12, 19))
     assert isinstance(masker.intervals, list)
 
 
@@ -27,19 +27,19 @@ def test_mask_hard():
 
 def test_repr():
     masker = DustMasker("GTACCCCCCCGTAACGTTTTT", window_size=64, score_threshold=20)
-    assert "DustMasker(sequence: 'GTACCCCC…', intervals: [(3, 10)])" == repr(masker)
+    assert "DustMasker(sequence: 'GTACCCCC…', intervals: ((3, 10)))" == repr(masker)
 
 
 def test_window_size():
     masker = DustMasker("TACCCCCCCGCGTTTTTTT", window_size=4, score_threshold=20)
     assert masker.window_size == 4
-    assert masker.intervals == []
+    assert masker.intervals == ()
 
 
 def test_score_threshold():
     masker = DustMasker("TACCCCCCCGCGTTTTTTT", window_size=64, score_threshold=128)
     assert masker.score_threshold == 128
-    assert masker.intervals == []
+    assert masker.intervals == ()
 
 
 def test_ambigious():
@@ -50,6 +50,6 @@ def test_ambigious():
     # with ambiguous and masks
     seq3 = "GCCAGGCTGGCCAAGGAGATTCttttttttttttttttttttttttAFGAGCCAGGCTGGCCAAGGAGANTCtttttttttnNnttttttttAAGA"
 
-    assert DustMasker(seq1, window_size=64).intervals == [(21, 45), (78, 102)]
-    assert DustMasker(seq2, window_size=64).intervals == [(22, 46), (72, 96)]
-    assert DustMasker(seq3, window_size=64).intervals == [(22, 46), (72, 81), (84, 92)]
+    assert DustMasker(seq1, window_size=64).intervals == ((21, 45), (78, 102))
+    assert DustMasker(seq2, window_size=64).intervals == ((22, 46), (72, 96))
+    assert DustMasker(seq3, window_size=64).intervals == ((22, 46), (72, 81), (84, 92))
