b5c2e0852face18ebdb2e94583cca42a6fd59c3a
Antonio Camargo <antoniop.camargo@gmail.com>
2025-12-15 14:33:22 -0300

    Update tests

--- tests/test_pydustmasker.py
+++ tests/test_pydustmasker.py
@@ -1,3 +1,5 @@
+import pytest
+
 from pydustmasker import DustMasker
 
 
@@ -8,6 +10,23 @@ def test_dust_masker_creation():
     assert masker.score_threshold == 20
     assert masker.intervals == ((2, 9), (12, 19))
     assert isinstance(masker.intervals, tuple)
+    assert len(masker) == 2
+
+
+def test_slicing():
+    masker = DustMasker("TACCCCCCCGCGTTTTTTT", window_size=64, score_threshold=20)
+    assert masker[0] == (2, 9)
+    assert masker[0:1] == ((2, 9),)
+    assert masker[:2] == ((2, 9), (12, 19))
+
+
+def test_iterable():
+    masker = DustMasker("TACCCCCCCGCGTTTTTTT", window_size=64, score_threshold=20)
+    assert hasattr(iter(masker), "__next__")
+    assert tuple(
+        DustMasker("TACCCCCCCGCGTTTTTTT", window_size=64, score_threshold=20)
+    ) == ((2, 9), (12, 19))
+    assert tuple(iter(masker)) == masker.intervals
 
 
 def test_n_masked_bases():
@@ -45,11 +64,29 @@ def test_score_threshold():
 def test_ambigious():
     # no ambiguous
     seq1 = "GCCAGGCTGGCCAAGGAGATCttttttttttttttttttttttttAAGAGACCATGGCATGCACTGGCCAAGGAGATCttttttttttttttttttttttttAAGA"
+    assert DustMasker(seq1, window_size=64).intervals == ((21, 45), (78, 102))
     # with ambiguous
     seq2 = "GCCAGGCTGGCCAAGGAGATTCttttttttttttttttttttttttAAGAGCCARYCTGGCCAAGGAGANTCttttttttttttttttttttttttAAGA"
+    assert DustMasker(seq2, window_size=64).intervals == ((22, 46), (72, 96))
     # with ambiguous and masks
     seq3 = "GCCAGGCTGGCCAAGGAGATTCttttttttttttttttttttttttAFGAGCCAGGCTGGCCAAGGAGANTCtttttttttnNnttttttttAAGA"
-
-    assert DustMasker(seq1, window_size=64).intervals == ((21, 45), (78, 102))
-    assert DustMasker(seq2, window_size=64).intervals == ((22, 46), (72, 96))
     assert DustMasker(seq3, window_size=64).intervals == ((22, 46), (72, 81), (84, 92))
+
+
+def test_errors_creation():
+    # sequence too short -> ValueError
+    with pytest.raises(ValueError):
+        DustMasker("AAA", window_size=64)
+    # window_size too small -> ValueError
+    with pytest.raises(ValueError):
+        DustMasker("ACGTACGT", window_size=3)
+    # negative integers for size/threshold should raise OverflowError when converting to unsigned types
+    with pytest.raises(OverflowError):
+        DustMasker("ACGTACGT", window_size=-1)
+    with pytest.raises(OverflowError):
+        DustMasker("ACGTACGT", window_size=64, score_threshold=-5)
+    # wrong types should raise TypeError
+    with pytest.raises(TypeError):
+        DustMasker(12345, window_size=64)
+    with pytest.raises(TypeError):
+        DustMasker("ACGTACGT", window_size="foo")
